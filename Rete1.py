770c7c71adceadadf96c0d8673754847b1bb0952
RiccardoRuberto <39532380+RiccardoRuberto@users.noreply.github.com>
2018-06-10 22:06:25 +0200

    Update Rete1.py

--- Rete1.py
+++ Rete1.py
@@ -1,55 +1,55 @@
 import random
 import time
-import MySQLdb
+import MySQLdb     #Libreria connessione database
 
-conn = MySQLdb.connect(host="localhost",user="root",passwd="",db="neuraldatas")
+conn = MySQLdb.connect(host="localhost",user="root",passwd="",db="neuraldatas")    #connessione al Db
 x = conn.cursor()
 
 rate = 0.01
 
-neurons = [0.0, 0.0]        
-weights = [random.uniform(0, 1), random.uniform(0, 1)]    # [input1, input2, somma/obbiettivo chiamate--> test_in[0] e test_in[1] 
+neuroni = [0.0, 0.0]        
+pesi = [random.uniform(0, 1), random.uniform(0, 1)]    # [input1, input2, somma/obbiettivo chiamate--> test_in[0] e test_in[1] 
 test = [ [4, 2, 6]    ]                                   # i pesi verranno aggiustati usando test_in[2] come obbiettivo.
-bias = 1
+errore = 1
 
 
 
 def output(threshold):      # Resterà a 1 finchè lavoreremo con s positivo.
-    global neurons, weights
-    s = neurons[0] * weights[0] + neurons[1] * weights[1] + bias
+    global neuroni, pesi
+    s = neuroni[0] * pesi[0] + neuroni[1] * pesi[1] + errore
     if s > threshold:
         return s
     return 0
 
-epoches = 0
+gen = 0
 while 1:
     for i in range(len(test)):  
-        neurons[1] = test[i][1]
+        neuroni[1] = test[i][1]
         out = output(2)
         # Pesi aggiustati in base alla formula:
         # Wnew = Wold + rate * [Obbiettivo - Attuale] * input
-        weights[0] = weights[0] + rate * (test[i][2] - out) * neurons[0]
-        weights[1] = weights[1] + rate * (test[i][2] - out) * neurons[1]
+        pesi[0] = pesi[0] + rate * (test[i][2] - out) * neuroni[0]
+        pesi[1] = pesi[1] + rate * (test[i][2] - out) * neuroni[1]
         # L'errore verrà corretto in base alla formula:
-        # Bias_new = Bias_old + (Obbiettivo - Attuale)
-        bias = bias + test[i][2] - out
+        # errore_new = errore_old + (Obbiettivo - Attuale)
+        errore = errore + test[i][2] - out
         if out == test[i][2]:     
             print("{0} ----> {1}!!!".format(out, test[i][2]))
 			
             time.sleep(5)
-            print("Generazioni necessarie = {0}".format(epoches))
+            print("Generazioni necessarie = {0}".format(gen))
 			
 			
         else:
             print("{0} dovrebbe essere {1}".format(out, test[i][2]))
-    epoches += 1
-    print("Pesetto1 = {0}\nPesetto2 è {1}".format(weights[0], weights[1]))
+    gen += 1
+    print("Pesetto1 = {0}\nPesetto2 è {1}".format(pesi[0], pesi[1]))
     print("-" * 79 + "\n\n")
-		x.execute("""INSERT INTO neuraldatas VALUES (peso1,peso2,risultato)""",(weights[0],weights[1],out))
-                conn.commit(
+		x.execute("""INSERT INTO neuraldatas VALUES (peso1,peso2,risultato)""",(pesi[0],pesi[1],out))
+                conn.commit()      #Inserimento dati nel Db
 	
 
-    if epoches % 10000 == 0:
-        print("Generazioni avvenute = {0}.".format(epoches))
+    if gen % 10000 == 0:
+        print("Generazioni avvenute = {0}.".format(gen))
 		
         time.sleep(3)
